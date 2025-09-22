3ffab7a2b4c15eb07ffa515176eea9c0d03c440e
Debashish-C <debashish.sahu.official@gmail.com>
2025-09-22 23:24:58 +0530

    day1 task completed

---
+++ Day1/CodePractice/practice.py
@@ -0,0 +1,15 @@
+# Data types
+x = 10
+y= 12.5
+name = "Rahul"
+isActive = True
+
+#check type
+print(type(x)) 
+
+age = 22
+name = "Debashish"
+
+# type casting
+
+age = int("22")
---
+++ Day1/Solution.py
@@ -0,0 +1,6 @@
+# Day 1: Python Basics - Task Solution
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print(f"Hello {name}, you are {age} years old!")
